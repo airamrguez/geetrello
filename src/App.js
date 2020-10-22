a3d619453b6708c312e4b9043287c2ebf401c395
Airam Rguez <airamrguezrivero@gmail.com>
2020-10-22 18:59:17 +0200

    Added project scaffolding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
+import AuthRoute from './components/AuthRoute';
+
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const SignupPage = lazy(() => import('./pages/SignupPage'));
+const BoardPage = lazy(() => import('./pages/BoardPage'));
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/signup">
+              <SignupPage />
+            </Route>
+            <AuthRoute path="/board">
+              <BoardPage />
+            </AuthRoute>
+            <Redirect exact from="/" to="/board" />
+          </Switch>
+        </Router>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AuthRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import auth from '../auth';
+
+export default function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: location } }} />
+        )
+      }
+    />
+  );
+}
